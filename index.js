dba91e461da8afd2793331d8ae13158d15f2c87a
Tom91011 <tomnightingale@hotmail.com>
2021-11-19 11:21:29 +0000

    Add mongoose, mongoDB and setup initial database

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 const express = require('express')
 const path = require('path')
-
 const app = express()
 const PORT = 9090
-
 
 app.set('view engine', 'ejs')
 app.use('/public', express.static(path.join(__dirname, './public')))
 
+const mongoose = require('mongoose')
+const mongoDB = 'mongodb://127.0.0.1/blogs_database';
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+const Schema = mongoose.Schema;
+
+let blogArray = []
+
+const blogSchema = new Schema ({
+  title: {
+    type: String,
+    maxLength: 50
+  },
+  content: String,
+  date: String,
+  image: String
+})
+
+const Blog = mongoose.model("Blog", blogSchema)
 
 app.get('/', (req, res) => {
-  res.render("home")
-})
+    res.render("home")
+  })
+
 
 app.get('/about', (req, res) => {
   res.render("about")
@@ -22,7 +43,14 @@ app.get('/services', (req, res) => {
 })
 
 app.get('/blogs', (req, res) => {
-  res.render("blogs")
+  // res.render("blogs")
+
+  Blog.find({}, (err, foundItems) => {
+    // console.log(foundItems);
+  res.render("blogs", {
+      foundItems: foundItems
+    })
+  })
 })
 
 app.get('/contact', (req, res) => {
