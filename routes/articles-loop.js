5e903aafa5dce926bea764a2e1855ff10aa83e5f
Tom91011 <tomnightingale@hotmail.com>
2022-01-06 18:22:41 +0000

    Split Blogs articles loop into it's own module

---
+++ routes/articles-loop.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const _ = require("lodash")
+const Article = require ('../controllers/Articlecontroller.js')
+const router = express.Router()
+const { getStartingPostion } = require ('../modules/starting-position.js')
+
+router.get('/', (req, res) => {
+    loadMoreClickCount = req.query.clicks
+    articlesToShow = 3
+    articlesCurrentlyShown += articlesToShow
+    articlesLeftToShow = totalArticles - articlesCurrentlyShown
+    console.log(articlesLeftToShow);
+    Article.find({}, (err, foundItems) => {
+      let loopArrayStartPosition = getStartingPostion(foundItems, loadMoreClickCount, articlesToShow)
+      let loopArrayEndPosition = foundItems.length - articlesCurrentlyShown
+      if(articlesLeftToShow <= 1) {
+        loopArrayEndPosition = 1
+      }
+  
+      res.render("articles-loop", {
+        articleArray: foundItems,
+        startingArticleArrayPostion: loopArrayStartPosition,
+        endingArticleArrayPosition: loopArrayEndPosition,
+        href:"/articles/"
+      })
+    })
+  })
+
+module.exports = router
