b95a1bdc88e3a23f74f012e23379587eafac4b34
Tom91011 <tomnightingale@hotmail.com>
2022-02-02 15:16:13 +0000

    move MongoDB access code to .env

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
+require('dotenv').config() 
+
 const mongoose = require('mongoose')
 // const mongoDB = 'mongodb://127.0.0.1/articles_database';
-const mongoDB = 'mongodb+srv://admin-tom:Test12385@cluster0.uc2hl.mongodb.net/articles_database';
+const mongoDB = process.env.MONGODB_DATABASE_ACCESS
 
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-console.log(mongoDB);
