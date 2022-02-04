abf3ad69681d85ecf15f8c59442f33c731237676
Tom91011 <tomnightingale@hotmail.com>
2022-02-04 16:45:10 +0000

    add files

--- public/character-counter.js
+++ public/character-counter.js
@@ -9,18 +9,12 @@ function charcountupdate() {
     const maxCharacterMessage = document.getElementById("max-character-message")
     const textBoxEl = document.getElementById("message")
     const textBoxTextContent = textBoxEl.value
-    console.log(maxCharacterMessage);
-    console.log(textBoxTextContent);
     let lng = textBoxEl.value.length
     textBoxEl.addEventListener("keyup", function() {
         let textBoxTextContent = textBoxEl.value
         let lng = textBoxEl.value.length
         maxCharacterMessage.textContent = lng + " of 1,000 characters"
-        console.log(textBoxTextContent);
-        console.log(lng);
     })
-	// document.getElementById("max-character-message").innerHTML = lng + ' out of 1000 characters';
-    // console.log("Here");
 }
 
 countCharacters()
